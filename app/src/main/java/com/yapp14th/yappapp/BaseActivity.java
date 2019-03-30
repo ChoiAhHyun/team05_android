ece27f6197c8f0af86846c2f708c3e7dd8dcc7b4
YKW93 <rhkddn1657@gmail.com>
2019-03-30 23:37:13 +0900

    Added BaseActivity function - 로딩 뷰 출력 기능 및 스타일, 레이아웃 추가

--- app/src/main/java/com/yapp14th/yappapp/BaseActivity.java
+++ app/src/main/java/com/yapp14th/yappapp/BaseActivity.java
@@ -1,7 +1,20 @@
 package com.yapp14th.yappapp;
 
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.widget.RelativeLayout;
 
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.annotation.IdRes;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -12,6 +25,11 @@ public abstract class BaseActivity extends AppCompatActivity {
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
+
+    private Dialog mProgressDialog;
+
+    private int mProgressCount = 0;
+    private boolean mIsShowProgress = false;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -33,7 +51,84 @@ public abstract class BaseActivity extends AppCompatActivity {
         ButterKnife.bind(this);
     }
 
-    @Nullable protected Toolbar getToolbar() {
+    @NonNull
+    protected Toolbar getToolbar() {
         return toolbar;
+    }
+
+    public void showProgress() {
+        if (mIsShowProgress) {
+            mProgressCount++;
+            return;
+        }
+
+        mProgressDialog = new Dialog(this, R.style.Loading_ProgressDialog);
+        if (mProgressDialog.getWindow() != null) {
+            mProgressDialog.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+        }
+        mProgressDialog.setCancelable(false);
+
+        RelativeLayout relativeLayout = (RelativeLayout) LayoutInflater.from(this).inflate(R.layout.view_loading, null, false);
+        mProgressDialog.setContentView(relativeLayout);
+        mProgressDialog.show();
+
+        mIsShowProgress = true;
+        mProgressCount = 0;
+    }
+
+    public void hideProgress() {
+        if (mIsShowProgress) {
+            if (mProgressCount != 0) {
+                mProgressCount--;
+            }
+            else {
+                if ((mProgressDialog != null) && mProgressDialog.isShowing()) {
+                    //get the Context object that was used to great the dialog
+                    Context context = ((ContextWrapper) mProgressDialog.getContext()).getBaseContext();
+
+                    // if the Context used here was an activity AND it hasn't been finished or destroyed
+                    // then dismiss it
+                    if (context instanceof Activity) {
+
+                        // Api >=17
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+                            if (!((Activity) context).isFinishing() && !((Activity) context).isDestroyed()) {
+                                dismissWithTryCatch(mProgressDialog);
+                            }
+                        }
+                        else {
+                            // Api < 17. Unfortunately cannot check for isDestroyed()
+                            if (!((Activity) context).isFinishing()) {
+                                dismissWithTryCatch(mProgressDialog);
+                            }
+                        }
+                    }
+                    else
+                        // if the Context used wasn't an Activity, then dismiss it too
+                        dismissWithTryCatch(mProgressDialog);
+                }
+                mProgressDialog = null;
+                mIsShowProgress = false;
+            }
+        }
+    }
+
+    private static void dismissWithTryCatch(Dialog dialog) {
+        try {
+            dialog.dismiss();
+        }
+        catch (final Exception e) {
+
+        }
+        finally {
+            dialog = null;
+        }
+    }
+
+    protected void customSnackBar(@IdRes int layout, String message, int duration) {
+        String html = "<font color=\"#ffffff\">" + message + "</font>\"";
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            Snackbar.make(findViewById(layout), Html.fromHtml(html, Html.FROM_HTML_MODE_LEGACY), duration).show();
+        }
     }
 }
