704be5f5f329967194e9c2343fd6d632af927334
ChoiAhHyun <cah1113@naver.com>
2019-03-31 01:12:30 +0900

    SwipeableCards 라이브러리 추가 및 ChooseInterestActivity 추가

---
+++ app/src/main/java/com/yapp14th/yappapp/item/InterestItem.java
@@ -0,0 +1,17 @@
+package com.yapp14th.yappapp.item;
+
+public class InterestItem {
+//    private String name;
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public InterestItem (String name) {
+//        this.name = name;
+//    }
+}
