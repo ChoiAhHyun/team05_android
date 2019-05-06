1d820ec2d0c45dbe32a2bc6984fd928735d58caf
ChoiAhHyun <cah1113@naver.com>
2019-05-07 01:31:18 +0900

    fragment_add.xml - image resource 추가 - UI 작업 미완성
    AddFragment.java
    - 위치 정보 TedPermission 추가 (추후에 제거)
    - meetingPlace[3] -> 만남 장소의 주소, 위도, 경도
    
    MapActivity.java
    - google map api 추가
    - 현재 위치 표시 및 marker 구현 완료
    - 장소 검색 관련 작업 보류
    
    activity_map.xml
    - 추후 수정

--- app/src/main/java/com/yapp14th/yappapp/view/fragment/AddFragment.java
+++ app/src/main/java/com/yapp14th/yappapp/view/fragment/AddFragment.java
@@ -1,15 +1,30 @@
 package com.yapp14th.yappapp.view.fragment;
 
+import android.Manifest;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
 import com.yapp14th.yappapp.Base.BaseFragment;
 import com.yapp14th.yappapp.R;
+import com.yapp14th.yappapp.view.activity.MapActivity;
+import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import butterknife.BindView;
 
 public class AddFragment extends BaseFragment {
+    private static final String TAG = "AddFragment";
+
+    String[] meetingPlace = new String[3];
+
+    @BindView(R.id.place_picker)
+    TextView place_picker;
 
     @Override
     protected int getLayout() {
@@ -23,5 +38,61 @@ public class AddFragment extends BaseFragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        getBaseActivity().setToolbar("모임 만들기", true);
+
+        TedPermission.with(getActivity())
+                .setPermissionListener(permissionlistener)
+                .setRationaleMessage("위치 정보 접근 권한이 필요합니다.")
+                .setDeniedMessage("[설정] > [권한] 에서 권한을 허용해주세요.")
+                .setPermissions(Manifest.permission.ACCESS_FINE_LOCATION)
+                .check();
+
+        place_picker.setOnClickListener(mOnClickListener);
+    }
+
+    PermissionListener permissionlistener = new PermissionListener() {
+        @Override
+        public void onPermissionGranted() {
+
+        }
+
+        @Override
+        public void onPermissionDenied(List<String> deniedPermissions) {
+
+        }
+    };
+
+    View.OnClickListener mOnClickListener = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.place_picker:
+                    Intent intent = new Intent(getActivity(), MapActivity.class);
+                    if (place_picker.getText().length() != 0) {
+                        intent.putExtra("lat", meetingPlace[1]);
+                        intent.putExtra("lng", meetingPlace[2]);
+                    }
+                    startActivityForResult(intent, 100);
+                    break;
+            }
+        }
+    };
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.d(TAG, "test1");
+        if (requestCode == 100) {
+            Log.d(TAG, "test2");
+            if (resultCode == 100) {
+                meetingPlace[0] = data.getStringExtra("address");
+                meetingPlace[1] = data.getStringExtra("lat");
+                meetingPlace[2] = data.getStringExtra("lng");
+                place_picker.setText(meetingPlace[0]);
+            }
+            else {
+                Log.d(TAG, resultCode + "");
+            }
+        }
     }
 }
